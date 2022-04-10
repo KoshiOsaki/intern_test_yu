002a1a581a3870c44aa05d644a4ed77f8e9689d1
Koshi Osaki <oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jpgit config --global user.email oosaki@futurewoods.co.jp>
2022-04-10 15:38:22 +0900

    軽微な修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,20 +12,18 @@ export const getStaticProps = async () => {
   try {
     //　都道府県リストの作成
     const prefList: Pref[] = [];
-    if (process.env.NEXT_PUBLIC_API_KEY) {
-      const headers = { "X-API-KEY": process.env.NEXT_PUBLIC_API_KEY };
-      const result = await axios.get(
-        "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-        {
-          headers: headers,
-        }
-      );
-      const data = result.data.result;
-      data.forEach((pref: Pref) => {
-        pref.prefCode = pref.prefCode - 1; //idを0からに統一
-        prefList.push(pref);
-      });
-    }
+    const headers = { "X-API-KEY": process.env.NEXT_PUBLIC_API_KEY as string };
+    const result = await axios.get(
+      "https://opendata.resas-portal.go.jp/api/v1/prefectures",
+      {
+        headers: headers,
+      }
+    );
+    const data = result.data.result;
+    data.forEach((pref: Pref) => {
+      pref.prefCode = pref.prefCode - 1; //idを0からに統一
+      prefList.push(pref);
+    });
 
     // 空の人口データリストの作成
     const defaultPopulationList: Population[] = [];
@@ -78,7 +76,7 @@ const Home: NextPage<Props> = ({ defaultPopulationList }: Props) => {
           <h1 className={styles.title}>都道府県の総人口推移</h1>
           <div className={styles.checkwrap}>
             {populationList.map((p: Population) => (
-              <CheckBox id={p.id} key={p.id}/>
+              <CheckBox id={p.id} key={p.id} />
             ))}
             <button className={styles.button} onClick={onClickReset}>
               選択をリセット
